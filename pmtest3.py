782200203dfd77d528fe90d920e4e9c456bcbfb1
leeshinhyeong <san2996@daum.net>
2019-09-05 19:08:00 +0900

    4th

--- pmtest3.py
+++ pmtest3.py
@@ -1,6 +1,17 @@
 import serial
 import time
 
+def db_insert(a, b, c) :
+    # DB Connect
+    conn = pymysql.connect(host='52.231.75.145', user='root', password='1234',db='mysql', charset='utf8')
+    curs = conn.cursor()
+
+    sql = """insert into dust_drond(gps_id, pm10Value, pm25Value, datecreated) values(%s, %s, %s, now())""";
+    curs.execute(sql, (a, b, c))
+    conn.commit()
+    conn.close()
+    #@sys.exit(1)
+
 def hexShow(argv):  
    result = ''  
    hLen = len(argv)  
@@ -26,13 +37,3 @@ while True:
                print "pm2.5:%.1f pm10 %.1f"%(pm25/10.0,pm10/10.0)
 
 
-def db_insert(a, b, c) :
-    # DB Connect
-    conn = pymysql.connect(host='52.231.75.145', user='root', password='1234',db='mysql', charset='utf8')
-    curs = conn.cursor()
-
-    sql = """insert into dust_drond(gps_id, pm10Value, pm25Value, datecreated) values(%s, %s, %s, now())""";
-    curs.execute(sql, (a, b, c))
-    conn.commit()
-    conn.close()
-    #@sys.exit(1)
